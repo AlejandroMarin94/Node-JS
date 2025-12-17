2183e1e20537055a749c0ae4829af3a2e5bd5e4e
Alejandro.Marin <4asjmamt@gmail.com>
2025-12-17 21:14:51 +0100

    ..

---
+++ REACT/EJEMPLOS AVANCE REACT REDUX/16 DIC/contador_prueba/contad/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import { CounterContext } from './CounterContent'
+
+function App() {
+
+
+
+  return (
+<CounterContext.Provider value={}>
+<h2>Datos compartidos por contexto</h2>
+
+</CounterContext.Provider>
+
+  )
+}
+
+export default App
